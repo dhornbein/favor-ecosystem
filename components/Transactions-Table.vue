7f4f16aa84d415a5ac969594e64cf160a90d3281
Drew <drew@dhornbein.com>
2021-12-17 11:51:34 -0700

    adds transactions table comp

---
+++ components/Transactions-Table.vue
@@ -0,0 +1,77 @@
+<template>
+  <table class="trans-table table-auto max-w-3xl w-full" v-if="trans">
+    <thead>
+      <tr>
+        <th v-for="title in trans.header" :key="title" :class="title">{{ title }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in trans.data" :key="row.ID" :id="`row-${row.ID}`" :title="row.timestamp"
+        class="odd:bg-gray-100"
+      >
+        <td v-for="(cell, col) in row" :key="row.ID + '-' + cell"
+          :class="col"
+        >
+          <details v-if="col == 'title' && row.description != ''">
+            <summary>{{ cell }}</summary>
+            <p>{{ row.description }}</p>
+          </details>
+          <span v-else>{{ cell }}</span>
+          
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    trans: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.trans-table {
+  @apply bg-white p-5 mx-auto;
+
+  tr {
+
+    th {
+      @apply p-4;
+    }
+    td {
+      @apply p-4;
+
+      &.amount,
+      &.fee {
+        @apply text-right font-mono;
+      }
+      &.fee {
+        @apply text-gray-600;
+      }
+    }
+  }
+
+  .ID,
+  .timestamp,
+  .payee_id,
+  .recipient_id,
+  .broker_name,
+  .ip_address,
+  .transaction_phone,
+  .effective_datetime,
+  .description {
+    @apply hidden;
+  }
+  .payee {}
+  .recipient {}
+  .title {
+    @apply max-w-sm;
+  }
+}
+</style>
