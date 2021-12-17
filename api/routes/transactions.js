13ac05a3112ae59ccbc4558bfe193779559e65a2
Drew <drew@dhornbein.com>
2021-12-17 13:19:17 -0700

    adds auth module

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -1,21 +1,14 @@
 const { Router } = require('express')
-const path = require('path');
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 const env = require('../env.json');
 const SPREADSHEET_ID = env.SPREADSHEET_ID;
-
-var serviceAccount = require("../serviceaccount.json");
+const auth = require('../auth');
 
 const router = Router()
 
 // Test route
 router.get('/transactions', function (req, res) {
-
-  const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, '../serviceaccount.json'),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
 
   google.options({ auth });
 
