70fae216c0aa708aa5c53d7672787433f327faa2
Drew <drew@dhornbein.com>
2021-12-17 23:20:59 -0700

    adds table view to member page

--- pages/members/index.vue
+++ pages/members/index.vue
@@ -6,14 +6,20 @@
       <header class="max-w-3xl mx-auto">
         <Logo />
         <h2 class="text-xl font-cormorant">Members</h2>
+        <a href="#table" @click="toggleView('table')" class="btn" :class="{ 'is-active': isTable}">Table</a>
+        <a href="#grid" @click="toggleView('grid')" class="btn" :class="{ 'is-active': isGrid}">Grid</a>
       </header>
       
     </div>
 
-    <div class="py-5 px-2 flex flex-wrap">
+    <div class="py-5 px-2 flex flex-wrap" v-if="isGrid">
       <div class="w-full md:w-1/2 lg:w-1/3 p-4" v-for="(row, idx) in members" :key="idx">
         <MemberCard class="h-full" :row="row" />
       </div>
+    </div>
+
+    <div class="py-5 px-2" v-if="isTable">
+      <DataTable :trans="members" :showCols="['ID','username','credit_limit','balance','transaction_total']" />
     </div>
 
 
@@ -30,6 +36,27 @@ export default {
     return {
       title: 'Users'
     }
-  }
+  },
+  data() {
+    return {
+      view: 'grid'
+    }
+  },
+  created() {
+    this.view = (this.$route.hash) ? this.$route.hash.slice(1) : this.view
+  },
+  computed: {
+    isGrid() {
+      return this.view === 'grid'
+    },
+    isTable() {
+      return this.view === 'table'
+    },
+  },
+  methods: {
+    toggleView(view) {
+      this.view = view
+    }
+  },
 }
 </script>
