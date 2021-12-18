f55e8ddb01a779a185d7473383308de024bb718a
Drew <drew@dhornbein.com>
2021-12-17 19:39:07 -0700

    moves call outside of router

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -6,35 +6,37 @@ const SPREADSHEET_ID = env.SPREADSHEET_ID;
 const auth = require('../auth');
 
 const router = Router()
+let headers = null
+let transactions = null
 const numberFields = ['ID', 'credit_limit', 'balance', 'credit', 'debit', 'transaction_total']
 
-// Test route
+google.options({ auth });
+
+sheets.spreadsheets.values.get({
+  spreadsheetId: SPREADSHEET_ID,
+  range: 'transactions!A:N',
+}, (err, result) => {
+  if (err) {
+    // Handle error
+    console.log(err);
+  } else {
+    headers = result.data.values.shift()
+    console.log(result.data.values);
+    transactions = result.data.values.map(row => {
+      let obj = {}
+      row.forEach((element, index) => {
+        obj[headers[index]] = numberFields.includes(headers[index]) ? parseFloat(element) : element
+      })
+      return obj
+    })
+  }
+})
+
 router.get('/transactions', function (req, res) {
 
-  google.options({ auth });
-
-  sheets.spreadsheets.values.get({
-    spreadsheetId: SPREADSHEET_ID,
-    range: 'transactions!A:N',
-  }, (err, result) => {
-    if (err) {
-      // Handle error
-      console.log(err);
-    } else {
-      let header = result.data.values.shift()
-      let data = result.data.values.map(row => {
-        let obj = {}
-        row.forEach((element, index) => {
-          obj[header[index]] = numberFields.includes(header[index]) ? parseFloat(element) : element
-        })
-        return obj
-      })
-
-      res.json({ 
-        header: header,
-        data: data
-      })
-    }
+  res.json({
+    header: headers,
+    data: transactions
   })
 
 })
