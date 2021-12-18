05f0714b3d1e370af4321d58b595f1ac73282ee6
Drew <drew@dhornbein.com>
2021-12-17 20:07:52 -0700

    adds ledger page

---
+++ pages/ledger.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    
+    <div class="lg:container mx-auto px-5">
+
+      <header class="max-w-3xl mx-auto">
+        <Logo />
+        <h2 class="text-xl font-cormorant">Ledger</h2>
+      </header>
+      
+      <div class="p-5">
+        <TransactionTable :trans="trans" />
+      </div>
+
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $http }) {
+    const trans = await $http.$get('/api/transactions')
+
+    return {
+      trans
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
