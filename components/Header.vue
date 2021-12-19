515e9767d5c7b458acc6f48095d836192029ea1b
Drew <drew@dhornbein.com>
2021-12-19 15:36:17 -0700

    adds mobile nav menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,12 @@
 <template>
-    <nav class="flex justify-center">
-      <nuxt-link :to="item.url" v-for="(item, idx) in nav" :key="idx"
-        class="mx-5 mb-5 px-10 bg-brand-primary text-white p-2 rounded-b-lg hover:bg-yellow-500 hover:text-brand-primary"
-      >{{ item.title }}</nuxt-link>
+    <nav class="nav" :class="{'is-active':showNav}">
+      <MemberIcon username="FSN" />
+      <div class="nav__menu">
+        <nuxt-link :to="item.url" v-for="(item, idx) in nav" :key="idx"
+          class=""
+        >{{ item.title }}</nuxt-link>
+      </div>
+      <BaseBurger :active="showNav" @click="showNav = !showNav" />
     </nav>
 </template>
 
@@ -10,25 +14,57 @@
   export default {
     data() {
       return {
+        showNav: true,
         nav: [
           {
-            title: 'Summary',
-            url: '/summary'
-          }, 
-          {
-            title: 'Ledger',
-            url: '/ledger'
-          }, 
+            title: 'Home',
+            url: '/'
+          },
           {
             title: 'transactions',
             url: '/transactions'
           }, 
           {
+            title: '+ Create',
+            url: '/transactions/create'
+          }, 
+          {
             title: 'Members',
             url: '/members'
+          },
+          {
+            title: '+ Create',
+            url: '/members/create'
           },
         ]
       }
     }
   }
 </script>
+
+<style lang="scss">
+.nav {
+  @apply bg-white flex gap-2 justify-between items-center p-2;
+
+  .nav__menu {
+    @apply hidden md:flex justify-between gap-2 px-2;
+
+    a {
+      @apply font-bold text-brand-primary hover:text-yellow-500;
+    }
+  }
+
+  &.is-active {
+    @apply flex-wrap;
+
+    .nav__menu {
+      @apply flex flex-col order-last w-full p-2 -mx-2 bg-gray-100;
+
+      a {
+        @apply font-bold text-brand-primary hover:text-yellow-500;
+      }
+    }
+  }
+    
+}
+</style>
---
+++ components/base/burger.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="burger" :class="{ 'is-active': active }" @click="$emit('click')">
+    Menu
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['active']
+}
+</script>
+
+<style lang="scss">
+.burger {
+  @apply font-bold;
+  &.is-active {
+    @apply text-brand-primary;
+  }
+}
+</style>
