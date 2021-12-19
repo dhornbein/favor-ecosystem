4795f6eb6871c6450e20e934619225e599d1df97
Drew <drew@dhornbein.com>
2021-12-19 02:14:07 -0700

    adds basic input

---
+++ components/base/input.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input" :class="$attrs.type" v-if="label">
+    <label :for="$attrs.id">{{ label }}</label>
+    <input v-bind="$attrs" :value="value" v-on:input="$emit('input', $event.target.value)">
+  </div>
+  <input v-else v-bind="$attrs" :value="value" v-on:input="$emit('input', $event.target.value)">
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: ['label', 'value']
+}
+</script>
+
+<style lang="scss" scoped>
+  .input {
+    @apply flex flex-col gap-2;
+
+    &.radio,
+    &.checkbox {
+      @apply flex-row-reverse justify-end items-baseline;
+    }
+    
+    input {
+      @apply border border-gray-200 shadow-inner rounded-lg p-2 focus:border-blue-200;
+    }
+
+  }
+</style>
