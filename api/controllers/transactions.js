b22dcb9408fe86bd64ade9175509cd026868e922
Drew <drew@dhornbein.com>
2021-12-21 09:54:36 -0700

    implements transaction id lookup

--- api/controllers/transactions.js
+++ api/controllers/transactions.js
@@ -4,8 +4,24 @@ exports.get = async (req, res, next) => {
 
   try {
     const trans = await transactions.get(req.params)
-    console.log('controller',trans);
-    res.status(200).send({ data: trans })
+
+    if (req.params.id) {
+      const found = trans.find(row => row.id === parseInt(req.params.id))
+      
+      if (found) {
+        res.status(200).json({ data: found })
+      } else {
+        res.status(404).json(error({
+          title: "Transaction Not Found",
+          detail: "No transactions found with that ID",
+          status: 404,
+          path: req.originalUrl,
+          timestamp: new Date(),
+        }))
+      }
+    } else {
+      res.status(200).json({ data: trans })
+    }
   } catch(err) {
     console.error(err)
   }
@@ -14,4 +30,10 @@ exports.get = async (req, res, next) => {
 
 exports.post = async (req, res, next) => {
   
+}
+
+function error(data){
+  return {
+    error: data
+  }
 }
--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -7,6 +7,7 @@ const use = fn => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
 router.get('/transactions', use(transactionsController.get));
+router.get('/transactions/:id', use(transactionsController.get))
 router.post('/transactions', use(transactionsController.post));
 
 // const { query } = require('express-validator/check');
