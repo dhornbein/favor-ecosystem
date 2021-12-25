8d4535dd967b18dc3466e8ac085cf6e2291b88a5
Drew <drew@dhornbein.com>
2021-12-25 00:00:49 -0700

    adds member api to schema

--- api/routes/members-router.js
+++ api/routes/members-router.js
@@ -10,7 +10,6 @@ const use = fn => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
 router.get('/members', auth.checkToken, use(membersController.get))
-router.get('/members/:id', auth.checkToken, use(membersController.get))
 router.post('/members', auth.requireToken, validate.isBroker, validate.member, use(membersController.post))
 router.post('/members/join/:token', auth.requireInviteToken, validate.inviteToken, validate.member, use(membersController.post), use(authController.claimInvite))
 
