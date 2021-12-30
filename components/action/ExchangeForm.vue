d5a48a2fbf6d906bb9832b0099c9b7080e877502
Drew <drew@dhornbein.com>
2021-12-30 02:07:41 -0700

    adds some formatting

--- components/action/ExchangeForm.vue
+++ components/action/ExchangeForm.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="my-4 flex-grow flex flex-col">
-    <input 
-      v-model="details.amount"
-      @change="setValues"
-      type="number"
-      placeholder="0.000" 
-      name=""
-      id=""
-      class="text-center text-4xl"
-    >
-    <input 
-      v-model="details.title"
-      @change="setValues"
-      type="text"
-      maxlength="140"
-      placeholder="Click to add Title"
-      class=""
-    >
-    <textarea 
-      v-model="details.description"
-      @change="setValues"
-      name="description"
-      placeholder="Optional description"
-      class="min-h-[2rem]focus:shadow-inner"
-    ></textarea>
+    <div class="field">
+      <input
+        v-model="details.amount"
+        @change="setValues"
+        type="number"
+        placeholder="0.000"
+        required
+        name=""
+        id=""
+        class="amount"
+      >
+    </div>
+    <div class="field">
+      <input
+        v-model="details.title"
+        @change="setValues"
+        type="text"
+        maxlength="140"
+        required
+        placeholder="Click to add Title"
+        class="title"
+      >
+      <div class="detail text-right text-sm text-gray-400">{{ details.title.length }}/140</div>
+    </div>
+    <div class="field">
+      <textarea
+        v-model="details.description"
+        @change="setValues"
+        name="description"
+        placeholder="Optional description"
+        class="description"
+      ></textarea>
+    </div>
     <!-- add broker drop down -->
   </div>
 </template>
@@ -46,13 +55,6 @@ export default {
     this.details = {
       ...this.$store.getters['exchange/details']
     }
-
-    // const unsubscribe = this.$store.subscribe((mutation, state) => {
-    //   console.log('mutate');
-    //   this.details = {
-    //     ...mutation.payload
-    //   }
-    // })
   },
   methods: {
     validate() {
@@ -63,26 +65,26 @@ export default {
       this.$store.dispatch('exchange/setDetails', this.details)
     },
   },
-  updated() {
-    this.details.payeeId = this.payeeId
-    this.details.recipientId = this.recipientId
-  },
-  computed: {
-    target() {
-      return this.$store.getters.getMemberByUid(this.$route.params.id)
-    },
-    setPayee() {
-      // this.details.payeeId = this.payeeId
-    },
-    setRecipient() {
-      // this.details.recipientId = this.recipientId
-    }
-  },
 }
 </script>
 
 <style lang="scss" scoped>
 input {
-  @apply p-2 my-4 w-full text-2xl border-b-2 border-gray-500 invalid:border-red-600;
+  @apply p-2 w-full text-2xl border-b-2 border-gray-500 invalid:border-red-200;
+}
+
+.field {
+  @apply my-4;
+}
+
+.amount {
+  @apply text-4xl text-center;
+}
+
+.title {
+}
+
+.description {
+  @apply min-h-[2rem] focus:shadow-inner;
 }
 </style>
