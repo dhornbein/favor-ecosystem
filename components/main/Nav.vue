44d01c05ce01e8333aea20320c86ab405f4937b1
Drew <drew@dhornbein.com>
2022-01-08 20:09:35 -0700

    moves nav to top

--- components/main/Nav.vue
+++ components/main/Nav.vue
@@ -1,13 +1,11 @@
 <template>
-  <footer class="w-screen p-2 bg-white shadow-lg border-t border-brand-primary z-30">
-    <div class="actions grid grid-cols-4 gap-2">
-      <nuxt-link to="/exchange" class="col-start-2 col-end-4 w-full text-center rounded-full bg-brand-primary text-yellow-500 font-bold px-4 text-2xl">Send Favor</nuxt-link>
-      <ChatBot class="ml-auto" />
-    </div>
-    <nav class="flex justify-around items-center mt-2 pt-2 border-t border-dotted border-yellow-500">
-      <nuxt-link to="/">Home</nuxt-link>
+  <header class="w-screen p-2 bg-white shadow-lg border-b border-brand-primary z-30">
+    <nav class="flex justify-around items-center">
+      <nuxt-link to="/">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+      </nuxt-link>
       <nuxt-link to="/members">Members</nuxt-link>
       <nuxt-link to="/transactions">Transactions</nuxt-link>
     </nav>
-  </footer>
+  </header>
 </template>
--- layouts/main.vue
+++ layouts/main.vue
@@ -1,14 +1,25 @@
 <template>
   <main class="view-layout">
-    <MainUserHeader class="header" />
-    <nuxt class="content" />
-    <MainNav class="foot fixed bottom-0 left-0 right-0" />
+    <MainNav ref="nav" class="foot fixed top-0 left-0 right-0" />
+    <nuxt class="content mb-12" :style="{ marginTop: navHeight + 'px'}" />
+    <div class="flex justify-between items-center fixed bottom-0 right-0 left-0 p-2">
+      <nuxt-link to="/exchange" class="text-center rounded-full bg-brand-primary text-yellow-500 font-bold px-4 py-2 text-2xl">Send Favor</nuxt-link>
+      <ChatBot class="w-12 h-12" />
+    </div>
   </main>
 </template>
 
 <script>
 export default {
-  middleware: 'load-data'
+  middleware: 'load-data',
+  data() {
+    return {
+      navHeight: 90,
+    }
+  },
+  mounted() {
+    this.navHeight = this.$refs.nav.$el.offsetHeight
+  },
 }
 </script>
 
