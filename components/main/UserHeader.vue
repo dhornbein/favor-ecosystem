7e68eade97fdb7419e42c1a854972039e08c4ab6
Drew <drew@dhornbein.com>
2022-01-12 14:29:44 -0700

    adds link to view onboarding

--- components/main/UserHeader.vue
+++ components/main/UserHeader.vue
@@ -1,6 +1,11 @@
 <template>
   <header v-if="$auth.user">
-    <MemberCard :size="view" ref="user" :member="$auth.user" />
+    <MemberCard :size="view" ref="user" :member="$auth.user">
+      <nuxt-link to="/onboard">
+        <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        Onboarding
+      </nuxt-link>
+    </MemberCard>
   </header>
 </template>
 
