70b5aaa4a4da8c82ad0a8098a4644ceb5164737c
Drew <drew@dhornbein.com>
2022-01-12 16:22:34 -0700

    updates pay buttons

--- pages/index/members/_username.vue
+++ pages/index/members/_username.vue
@@ -2,7 +2,10 @@
   <main>
     
     <MemberCard :member="member" size="open">
-      <nuxt-link :to="`/exchange/pay/${member.username}`" class="btn btn-sm" v-if="!isMe">Pay {{ member.firstName }}</nuxt-link>
+      <nuxt-link :to="`/exchange/pay/${member.username}`" class="btn has-text-sm is-primary is-rounded-full" v-if="!isMe">
+        <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+        Pay {{ member.firstName }}
+      </nuxt-link>
     </MemberCard>
 
     <h2 class="text-xl font-cormorant">{{ member.firstName }}'s Recent Transactions</h2>
