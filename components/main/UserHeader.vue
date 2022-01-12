02b857be9f4bf077f2f498f69682b3664c12341f
Drew <drew@dhornbein.com>
2022-01-12 16:04:25 -0700

    moves logout button

--- components/main/UserHeader.vue
+++ components/main/UserHeader.vue
@@ -1,10 +1,14 @@
 <template>
   <header v-if="$auth.user">
     <MemberCard :size="view" ref="user" :member="$auth.user">
-      <nuxt-link to="/onboard">
-        <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      <nuxt-link to="/onboard" class="flex gap-2">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
         Onboarding
       </nuxt-link>
+      <button class="flex gap-2" @click="$auth.logout()">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+        Logout
+      </button>
     </MemberCard>
   </header>
 </template>
--- components/member/Card.vue
+++ components/member/Card.vue
@@ -15,19 +15,18 @@
         <BaseFavor class="text-right open-hidden pr-2 border-r border-gray-400" :num="member.creditLimit + member.balance">
           <div class="label text-xs">Available</div>
         </BaseFavor>
-        <button class="btn btn-sm" @click="$auth.logout()" v-if="isCurrentUser">Logout</button>
       </div>
     </header>
 
     <main class="body">
-      <div class="details">
+      <div class="details flex flex-col gap-2">
         <div class="email" @click="$emit('emailClick')">{{ member.email }}</div>
         <div class="phone" @click="$emit('phoneClick')">{{ member.phone }}</div>
-        <div class="edit text-gray-700 mt-4 flex gap-2" @click="$emit('editClick')" v-if="isCurrentUser">
+        <div class="edit text-gray-700 mt-auto flex gap-2" @click="$emit('editClick')" v-if="isCurrentUser">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
           Edit Your Profile
         </div>
-        <div class="mt-2">
+        <div class="flex flex-col gap-2">
           <slot></slot>
         </div>
       </div>
@@ -108,7 +107,7 @@ export default {
     @apply flex gap-2 items-center;
   }
   .body {
-    @apply flex gap-2 justify-between;
+    @apply flex gap-2 justify-between mt-2;
     .balance {
       @apply text-right;
     }
