0a2417e4d24afb9cab44dbcdbe21fd22c0f1abf3
Drew <drew@dhornbein.com>
2022-01-15 19:28:25 -0700

    makes loader fixed

--- components/base/Loader.vue
+++ components/base/Loader.vue
@@ -9,7 +9,7 @@
 
 <style lang="scss">
 .loader {
-  @apply absolute inset-0 z-50 flex flex-col gap-4 justify-center items-center w-full h-full bg-white bg-opacity-80 text-4xl text-purple-900;
+  @apply fixed inset-0 z-50 flex flex-col gap-4 justify-center items-center w-screen h-screen bg-white bg-opacity-80 text-4xl text-purple-900;
   .spinner {
     @apply animate-spin inline-block w-16 h-16 border-4 rounded-full border-purple-900;
     border-right-color: transparent;
