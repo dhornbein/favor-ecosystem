402ad204432a633bf42307acbd4c5246946ee606
Drew <drew@dhornbein.com>
2022-01-29 13:33:27 -0700

    login error handling

---
+++ components/base/Error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="error-box">
+    <p class="error-box__title">Error!</p>
+    <div class="error-box__message">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.error-box {
+  @apply p-4 border-l-2 border-red-500 bg-red-100;
+  .error-box__title {
+    @apply text-red-500 font-bold;
+  }
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -4,23 +4,48 @@
 
     <h1 class="text-2xl my-4">Log In</h1>
 
-    <form @submit.prevent="userLogin">
-      <div>
-        <BaseInput label="Username" type="text" v-model="login.username" required />
-      </div>
-      <div>
-        <BaseInput label="Password" type="password" v-model="login.password" required />
-      </div>
+    <ValidationObserver 
+      class="login-form form"
+      ref="form"
+      v-slot="{ handleSubmit }"
+    >
+
+      <BaseError class="error mb-4" v-if="loginError">{{ loginError }}</BaseError>
+    
+      <ValidationProvider slim rules="required" v-slot="{ errors, classes }">
+        <div class="control" :class="classes">
+          <BaseInput label="Username" type="text" v-model="login.username" required />
+          <div class="input-meta">
+            <span class=errors>{{ errors[0] }}</span>
+          </div>
+        </div>
+      </ValidationProvider>
+
+      <ValidationProvider slim rules="required" v-slot="{ errors, classes }">
+        <div class="control" :class="classes">
+          <BaseInput label="Password" type="password" v-model="login.password" required />
+          <div class="input-meta">
+            <span class=errors>{{ errors[0] }}</span>
+          </div>
+        </div>
+      </ValidationProvider>
+
       <div class="py-4">
-        <button class="btn" type="submit">Submit</button>
+        <button class="btn is-primary text-2xl my-4" type="submit" @click.prevent="handleSubmit(userLogin)">Login</button>
       </div>
-    </form>
+
+    </ValidationObserver>
     <p>Don't have an account? <nuxt-link to="/join" class="font-bold text-brand-primary">Join Favor!</nuxt-link></p>
   </div>
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   head: {
     title: 'Log In'
   },
@@ -30,7 +55,8 @@ export default {
       login: {
         username: '',
         password: ''
-      }
+      },
+      loginError: false
     }
   },
   methods: {
@@ -38,15 +64,21 @@ export default {
       try {
         this.loading = true;
         let response = await this.$auth.loginWith('local', { data: this.login })
+
         this.$store.dispatch('chat/broadcastResponse', {
           response: response,
           title: 'login successful',
         })
+
         this.loading = false
       } catch (error) {
-        this.$store.dispatch('chat/broadcastErrorResponse', {
-          response: error.response,
-        })
+        if (error.response.data.error) {
+          this.loginError = error.response.data.error[0].msg
+        } else {
+          // TODO unify error handling response
+          this.loginError = error.response.data.title || error.response.data
+        }
+        
         this.loading = false
       }
     }
